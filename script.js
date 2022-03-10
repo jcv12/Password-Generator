6f63d51bef50bc0f7839fb79d998b515a94511d1
jcv12 <john.c.vayianos@gmail.com>
2022-03-10 18:39:02 -0500

    created all variables

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var generateBtn = document.querySelector("#generate");
 
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
